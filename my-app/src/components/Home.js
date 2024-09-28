60e32f3dc0361804617cbec127b58b7c63896cd9
Varsha Karan Kumaar <varshalathakumar.com>
2024-09-28 15:14:38 +0530

    second

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,8 +3,10 @@ import React from  'react'
 function Home(){
     return(
     <div>
-        <h1>HOME PAGE</h1>
+        <h1>HOME tfyguhgweilkePAGE</h1>
     </div>
     );
 }
 export default Home;
+
+

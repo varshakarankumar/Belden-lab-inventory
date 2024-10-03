57635fd47b1ba59e50dcabef0f0a05a30e84d590
Varsha Karan Kumaar <varshalathakumar.com>
2024-10-03 18:43:04 +0530

    fourth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,78 @@ import Navbar from './components/Navbar';
 function App() {
   return (
      
-        <div className="App">
-          <Navbar/>
-          Learn React guikgj
-        </div>
+    <div className="App">
+    <Navbar />
+    <h1>Request Form</h1>
+    <form>
+      <label>
+        Request No.:
+        <input type="text" name="requestNo" required />
+      </label><br/>
+
+      <label>
+        Owner SC:
+        <input type="text" name="ownerSC" required />
+      </label><br/>
+
+      <label>
+        Requester:
+        <input type="text" name="requester" required />
+      </label><br/>
+
+      <label>
+        Distributor:
+        <input type="text" name="distributor" required />
+      </label><br/>
+
+      <label>
+        End Customer:
+        <input type="text" name="endCustomer" required />
+      </label><br/>
+
+      <label>
+        Contact:
+        <input type="text" name="contact" required />
+      </label><br/>
+
+      <label>
+        Request Date:
+        <input type="date" name="requestDate" required />
+      </label><br/>
+
+      <label>
+        Return Date:
+        <input type="date" name="returnDate" required />
+      </label><br/>
+
+      <label>
+        Status:
+        <input type="text" name="status" required />
+      </label><br/>
+
+      <label>
+        Close Date:
+        <input type="date" name="closeDate" required />
+      </label><br/>
+
+      <label>
+        Material/Serial:
+        <input type="text" name="materialSerial" required />
+      </label><br/>
+
+      <label>
+        Location Address:
+        <input type="text" name="locationAddress" required />
+      </label><br/>
+
+      <label>
+        Remarks:
+        <input type="text" name="remarks" />
+      </label><br/>
+
+      <button type="submit">Submit</button>
+    </form>
+  </div>
   );
 }
 
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,7 +3,7 @@ import React from  'react'
 function Home(){
     return(
     <div>
-        <h1>HOME PAGE</h1>
+        <h1>HOME PAGE uyafgwbaognk</h1>
     </div>
     );
 }
